23cb4672ff4d92321f563cf2fe1ba692e395032e
Ruben <rubenski@gmail.com>
2020-08-23 22:17:21 +0200

    Added comments to modal service

--- src/app/demo.app.modal.service.ts
+++ src/app/demo.app.modal.service.ts
@@ -4,14 +4,20 @@ import {CountryDataProvider} from './provider/country.data.provider';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 /**
- * Provides a sharable reference to the modal dom element
+ * This service provides a link between the modal popup created from AppComponent and its ouput (consent or no consent), which is needed
+ * in HomeComponent.
  */
 @Injectable()
 export class DemoAppModalService {
 
   private countryDataProvider = new Subject<CountryDataProvider>();
   private modalClosedEvent = new Subject<string>();
+
+  // When the user selects a country data provider, AppComponent will be able to launch the consent modal,
+  // because it subscribes to countryDataProviderObservable
   countryDataProviderObservable = this.countryDataProvider.asObservable();
+  // When the user provides consent in the modal, or closes the modal, HomeComponent will know by subscribing to modelClosedEventObservable
+  // This will allow it to redirect the user to the bank in case of user consent
   modelClosedEventObservable = this.modalClosedEvent.asObservable();
 
   constructor(private modalService: NgbModal) {
